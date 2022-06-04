2a66a1530a7721a8ea7f66883850911c3f1852bc
Administrator <Administrator@804-t>
2022-06-04 17:07:41 +0900

    1111

--- SpringBootJPAProject_3/src/main/java/com/it/music/controller/MusicRestController.java
+++ SpringBootJPAProject_3/src/main/java/com/it/music/controller/MusicRestController.java
@@ -1,5 +1,22 @@
 package com.it.music.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.it.music.dao.*;
+import com.it.music.entity.*;
+@RestController
 public class MusicRestController {
-
+   // JSON
+	@Autowired
+	private MusicDAO dao;
+	
+	@PostMapping("/detail")
+	public MusicEntity music_detail(int no)
+	{
+		MusicEntity vo=dao.findByNo(no);
+		System.out.println(vo);
+		return vo;
+	}
+   
 }
